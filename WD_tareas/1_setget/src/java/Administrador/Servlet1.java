ca727e12c7c2f8267c8c6106a79c69b02d1fba92
JJZEUS55 <jjzeus55@gmail.com>
2017-08-15 00:57:38 -0500

    set y get HTML obtener parametros de html para pasar a servlet

---
+++ WD_tareas/1_setget/src/java/Administrador/Servlet1.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Administrador;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Jorge :v
+ */
+public class Servlet1 extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        String parametro1 = request.getParameter("parametro1");
+        String parametro2 = request.getParameter("parametro2");
+        HttpSession session = request.getSession();
+        session.setAttribute("atributo1", parametro1);
+        session.setAttribute("atributo2", parametro2);
+        
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet hola1</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            //out.println("<h1>Servlet hola1 at " + request.getContextPath() + "</h1>");
+            out.println("<h2>Hola prro :3<h2>");
+            out.println("</body>");
+            out.println("</html>");
+    }
+
+ 
+
+}
+
