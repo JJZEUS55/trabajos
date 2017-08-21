e8a46dbb000cf1a328801c347448de409ba3f2f7
JJZEUS55 <jjzeus55@gmail.com>
2017-08-21 13:00:09 -0500

    removeAtributte
    -Remover atributos a nivel aplicacion
    -se puede usar para poder eliminar la sesion

---
+++ WD_tareas/11_Session_atributos/src/java/admin/Servlet0.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package admin;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Jorge :v
+ */
+public class Servlet0 extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        
+        ServletContext application = this.getServletContext();
+        application.removeAttribute("clave1");
+        
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Servlet hola1</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h2>Hola prro :3<h2>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
