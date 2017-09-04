bb32b7f9bd35259db71c1d1fbf13901196a90e90
JJZEUS55 <jjzeus55@gmail.com>
2017-09-04 13:28:12 -0500

    getResourcePaths -Obtiene las rutas relativas de todos los html

---
+++ WD_tareas/26_getResourcePath/src/java/admin/Servlet2.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package admin;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Set;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Jorge :v
+ */
+public class Servlet2 extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        ServletContext application = this.getServletContext();
+        Set conjunto = application.getResourcePaths("/");//Se refiere a la ruta relativa de la aplicacion web
+
+        /* TODO output your page here. You may use following sample code. */
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Servlet hola1</title>");
+        out.println("</head>");
+        out.println("<body>");
+        //out.println("<h1>Servlet hola1 at " + request.getContextPath() + "</h1>");
+        out.println("<h2> Servelet getResourcePaths" + conjunto.toString() + "<h2>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+
+}
