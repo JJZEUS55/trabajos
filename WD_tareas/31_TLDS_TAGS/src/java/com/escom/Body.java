e3cb48543195fd67b6120151349422610d1c9d11
JJZEUS55 <jjzeus55@gmail.com>
2017-10-14 09:16:55 -0500

    .
    .

--- WD_tareas/31_TLDS_TAGS/src/java/com/escom/Body.java
+++ WD_tareas/31_TLDS_TAGS/src/java/com/escom/Body.java
@@ -5,55 +5,48 @@ import javax.servlet.jsp.*;
 import javax.servlet.jsp.tagext.*;
 import java.util.*;
 
-public class Body extends BodyTagSupport
-{
-    protected String elemSep =  ",";
+public class Body extends BodyTagSupport {
+
+    protected String elemSep = ",";
     protected String lineSep = ";";
 
-    public int doEndTag() throws JspException
-    {
-       String bodyText = bodyContent.getString();
+    public int doEndTag() throws JspException {
+        String bodyText = bodyContent.getString();
 
-       try {
-        pageContext.getOut().print(bodyText);
-       }
-       catch (Exception e) {
-        throw new JspTagException(e.getMessage());
-       }
+        try {
+            pageContext.getOut().print(bodyText);
+        } catch (Exception e) {
+            throw new JspTagException(e.getMessage());
+        }
 
-       StringBuffer tableOut = new StringBuffer();
-       StringTokenizer bodyTk = new StringTokenizer(bodyText, lineSep, false);
+        StringBuffer tableOut = new StringBuffer();
+        StringTokenizer bodyTk = new StringTokenizer(bodyText, lineSep, false);
 
-       tableOut.append("<TABLE>");
-       while (bodyTk.hasMoreTokens())
-       {
-           String str = (String)bodyTk.nextToken();
-           StringTokenizer token = new StringTokenizer(str, elemSep, false);
-           tableOut.append("<TR>");
-           while(token.hasMoreTokens())
-           {
+        tableOut.append("<TABLE border='1'>");
+        while (bodyTk.hasMoreTokens()) {
+            String str = (String) bodyTk.nextToken();
+            StringTokenizer token = new StringTokenizer(str, elemSep, false);
+            tableOut.append("<TR>");
+            while (token.hasMoreTokens()) {
                 tableOut.append("<TD>");
-                tableOut.append((String)token.nextToken());
+                tableOut.append((String) token.nextToken());
                 tableOut.append("</TD>");
-           }
-           tableOut.append("</TR>");
-       }
-      tableOut.append("</TABLE>");
+            }
+            tableOut.append("</TR>");
+        }
+        tableOut.append("</TABLE>");
 
-      try {
-        pageContext.getOut().print(tableOut);
-       }
-       catch (Exception e) {
-        throw new JspTagException(e.getMessage());
-       }
-       return EVAL_PAGE;
+        try {
+            pageContext.getOut().print(tableOut);
+        } catch (Exception e) {
+            throw new JspTagException(e.getMessage());
+        }
+        return EVAL_PAGE;
     }
 
-    public void release()
-    {
-       elemSep = ",";
-       lineSep = ";";
+    public void release() {
+        elemSep = ",";
+        lineSep = ";";
     }
 
 }
-
